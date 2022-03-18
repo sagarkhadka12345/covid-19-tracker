ef3db7f2f42d55997ca2add3ea677e4fa208355c
sagar12345 <sagar.khadka2001@gmail.com>
2022-03-18 08:20:01 +0545

    covid tracker

---
+++ app.js
@@ -0,0 +1,238 @@
+
+// SELECT ALL ELEMENTS
+const country_name_element = document.querySelector(".country .name");
+const total_cases_element = document.querySelector(".total-cases .value");
+const new_cases_element = document.querySelector(".total-cases .new-value");
+const recovered_element = document.querySelector(".recovered .value");
+const new_recovered_element = document.querySelector(".recovered .new-value");
+const deaths_element = document.querySelector(".deaths .value");
+const new_deaths_element = document.querySelector(".deaths .new-value");
+
+const ctx = document.getElementById("axes_line_chart").getContext("2d");
+let value = document.getElementById("recval");
+let n5= document.getElementById('dailycase');
+     let n6= document.getElementById('dailydeath');
+     let n7= document.getElementById('dailyrecovered');
+
+// APP VARIABLES
+let app_data = [],
+  cases_list = [],
+  recovered_list = [],
+  deaths_list = [],
+  deaths = [],
+  formatedDates = [],
+  new_recovered_cases=[];
+
+// GET USERS COUNTRY CODE
+fetch("https://api.ipgeolocation.io/ipgeo?apiKey=14c7928d2aef416287e034ee91cd360d")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    let country_code = data.country_code2;
+    let user_country;
+    country_list.forEach((country) => {
+      if (country.code == country_code) {
+        user_country = country.name;
+      }
+    });
+    fetchData(user_country);
+  });
+
+/* ---------------------------------------------- */
+/*                     FETCH API                  */
+/* ---------------------------------------------- */
+function fetchData(country) {
+  user_country = country;
+  country_name_element.innerHTML = "Loading...";
+
+  (cases_list = []),
+    (recovered_list = []),
+    (deaths_list = []),
+    (dates = []),
+    (formatedDates = []);
+    (new_recovered_cases =[]);
+
+  var requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+console.log(country)
+
+  const api_fetch = async (country) => {
+    await fetch(
+      "https://api.covid19api.com/total/country/" + country + "/status/confirmed",
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          dates.push(entry.Date);
+          cases_list.push(entry.Cases)
+          // new_recovered_cases.push(d);
+        });
+      });
+
+      await fetch('https://covid-19.dataflowkit.com/v1/' + country).then(response=>response.json()).then(result=>res(result))
+      function res(update){
+       console.log(update);
+       let d = update["Total Recovered_text"];
+      value.innerHTML =d;
+       
+      }
+      await fetch(
+        "https://api.covid19api.com/total/country/" + country ,
+        requestOptions
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          data.forEach((entry) => {
+            recovered_list.push(entry.Active);
+            // recovered_list.push('[801722]')
+           
+          });
+        });
+
+    await fetch(
+      "https://api.covid19api.com/total/country/" + country + "/status/deaths",
+      requestOptions
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        data.forEach((entry) => {
+          deaths_list.push(entry.Cases);
+        });
+      });
+
+    updateUI();
+  };
+
+  api_fetch(country);
+}
+
+let new_recovered_list=[];
+let new_arr = new_recovered_list.concat(recovered_list);
+// UPDATE UI FUNCTION
+function updateUI() {
+  updateStats();
+  axesLinearChart();
+}
+
+function updateStats() {
+  const total_cases = cases_list[cases_list.length - 1];
+  const new_confirmed_cases = total_cases - cases_list[cases_list.length - 2];
+
+ 
+  const new_total_recovered = recovered_list[recovered_list.length -1];
+  const new_recovered_cases = new_total_recovered - recovered_list[recovered_list.length - 2];
+  
+
+  const total_deaths = deaths_list[deaths_list.length - 1];
+  const new_deaths_cases = total_deaths - deaths_list[deaths_list.length - 2];
+
+  country_name_element.innerHTML = user_country;
+  total_cases_element.innerHTML = total_cases;
+  new_cases_element.innerHTML = `+${new_confirmed_cases}`;
+  n5.innerHTML = "+197";
+  //recovered_element.innerHTML = new_total_recovered  ;
+  n7.innerHTML = "+325";
+  deaths_element.innerHTML = total_deaths;
+  new_deaths_element.innerHTML = `+${new_deaths_cases}`;
+  n6.innerHTML = "+3";
+
+  // format dates
+  dates.forEach((date) => {
+    formatedDates.push(formatDate(date));
+  });
+}
+
+// UPDATE CHART
+let my_chart;
+function axesLinearChart() {
+  if (my_chart) {
+    my_chart.destroy();
+  }
+
+  my_chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      datasets: [
+        {
+          label: "Cases",
+          data: cases_list,
+          fill: false,
+          borderColor: "#FFF",
+          backgroundColor: "#FFF",
+          borderWidth: 1,
+        },
+        {
+          label: "Recovered",
+          data: recovered_list,
+          fill: false,
+          borderColor: "#009688",
+          backgroundColor: "#009688",
+          borderWidth: 1,
+        },
+        {
+          label: "Deaths",
+          data: deaths_list,
+          fill: false,
+          borderColor: "#f44336",
+          backgroundColor: "#f44336",
+          borderWidth: 1,
+        },
+      ],
+      labels: formatedDates,
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  });
+}
+
+// FORMAT DATES
+const monthsNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+function formatDate(dateString) {
+  let date = new Date(dateString);
+
+  return `${date.getDate()} ${monthsNames[date.getMonth()]}`;
+}
+
+fetch("https://google-news.p.rapidapi.com/v1/top_headlines?lang=en&country=US", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "google-news.p.rapidapi.com",
+		"x-rapidapi-key": "5383fa6653mshe48350ffe07fa8fp13927ejsn7dffbd9cf2fb"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+
+// console.log(con);
